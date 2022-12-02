b947a9619fed86b073452a3d5c15a02ce5471259
James Yanyuk <james@yanyuk.com>
2022-12-02 01:59:12 -0500

    Fixed padding and duplicate domains

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home({ domains }) {
 
   return (
     <div className="h-screen flex">
-      <div className="fade p-[120px] m-auto">
+      <div className="fade container m-auto">
         <a
           href="https://yanyuk.com/"
           className="inline-block w-[100px] h-[42px] mb-[36px] mr-[18px] indent-[-9999px] bg-[url('/yanyuk.png')] bg-contain bg-left bg-no-repeat"
@@ -119,9 +119,6 @@ export async function getServerSideProps(context) {
 
   // Dedupe domains
   domains = [...new Set(domains)];
-
-  //TODO: Remove
-  domains = [...domains, ...domains, ...domains, ...domains, ...domains];
 
   return {
     props: {
